9911fa35085a983536cec44a1197704b06a72ec7
Silviu Marinca <silviu_marinca@yahoo.com>
2020-07-10 12:08:49 +0300

    ProjectOrganization

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _ctx;
+        public ProductsController(StoreContext ctx)
+        {
+            _ctx = ctx;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products =await _ctx.Products.ToListAsync();
+
+            return     Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>>   getProduct(int id)
+        {
+
+            return await _ctx.Products.FindAsync(id);
+        }
+    }
+}
