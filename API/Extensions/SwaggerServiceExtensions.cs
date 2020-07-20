5d8df5a624091d1cb3dbd91c78bb58ae857eaf35
Silviu Marinca <silviu_marinca@yahoo.com>
2020-07-20 18:33:16 +0300

    Swagger

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace API.Extensions
 {
@@ -11,7 +12,28 @@ namespace API.Extensions
 
             services.AddSwaggerGen(c=>{
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo{ Title="SKINET APi", Version="v1"});
-
+            var security = new OpenApiSecurityScheme()
+            {
+                Description="JWT auth Bearer scheme",
+                Name ="Authorization",
+                In =ParameterLocation.Header,
+                Type=SecuritySchemeType.Http,
+                Scheme="bearer",
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                } 
+            };
+                     c.AddSecurityDefinition("Bearer",security);
+               var securityRequirement = new OpenApiSecurityRequirement {{
+                      security,
+                      new []{"Bearer"}
+                      }};
+                      
+                   c.AddSecurityRequirement(securityRequirement);
+                   
+                  
             });
             return services;
            }
