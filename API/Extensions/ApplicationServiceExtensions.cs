e94497553d800f9e3d384f578476b3f70da89b04
Silviu Marinca <silviu_marinca@yahoo.com>
2020-07-22 16:14:22 +0300

    Automapper and checkout

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,8 @@ namespace API.Extensions
             services.AddScoped<IProductRepository,ProductRepository>();
             services.AddScoped<IBasketRepository,BaskeRepository>();
             services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddScoped<IOrderService,OrderService>();
+             services.AddScoped<IUnitOfWork,UnitOfWork>();
              services.Configure<ApiBehaviorOptions>(options=>
             {
                 options.InvalidModelStateResponseFactory = actionContext =>{
